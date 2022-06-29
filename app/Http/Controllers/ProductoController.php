7f5079a7134f07d5296820329a1f4bd8b6e01878
cchura94 <cchura.cpc@gmail.com>
2022-06-28 22:06:31 -0400

    Migracion models controller crud-productos

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // /api/producto?page=1
+        $productos = Producto::paginate(10);
+
+        return response()->json($productos, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "nombre" => "required|min:3|max:200",
+            "categoria_id" => "required"
+        ]);
+
+        // guardar
+        $producto = new Producto;
+        $producto->nombre = $request->nombre;
+        $producto->stock = $request->stock;
+        $producto->precio = $request->precio;
+        $producto->descipcion = $request->descipcion;
+        $producto->categoria_id = $request->categoria_id;
+        $producto->save();
+
+        return response()->json(["mensaje" => "producto registrado"], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producto $producto)
+    {
+        return response()->json($producto, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        $request->validate([
+            "nombre" => "required|min:3|max:200",
+            "categoria_id" => "required"
+        ]);
+        
+        // modificamos
+        $producto->nombre = $request->nombre;
+        $producto->stock = $request->stock;
+        $producto->precio = $request->precio;
+        $producto->descipcion = $request->descipcion;
+        $producto->categoria_id = $request->categoria_id;
+        $producto->save();
+        return response()->json(["mensaje" => "producto modificado"], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+        return response()->json(["mensaje" => "producto eliminado"], 201);
+    }
+}
---
+++ database/migrations/2022_06_29_010436_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre_completo", 100);
+            $table->string("ci_nit", 18)->nullable();
+            $table->string("telefono", 15)->nullable();
+            $table->string("direccion")->nullable();
+
+            $table->softDeletes(); // deleted_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
