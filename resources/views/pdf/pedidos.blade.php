0917fc65c59033c92b4f4acdc4e41cb9a2a7494a
cchura94 <cchura.cpc@gmail.com>
2022-07-14 17:21:14 -0400

    pdf

--- resources/views/pdf/pedidos.blade.php
+++ resources/views/pdf/pedidos.blade.php
@@ -1,4 +1,4 @@
-
+<h1 class="titulo">Lista Pedidos</h1>
 <table border="1">
     <tr>
         <td>ID</td>
@@ -16,3 +16,31 @@
     @endforeach
 
 </table>
+
+<style>
+    .titulo{
+        color: blue;
+    }
+</style>
+
+<img width="400px" src="https://quickchart.io/chart?c={type:'bar',data:{labels:[2012,2013,2014,2015,2016],datasets:[{label:'Users',data:[120,60,50,180,120]}]}}" alt="">
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.js"></script>
+
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, 
+{
+  type: 'bar',                                // Show a bar chart
+  data: {
+    labels: [2012, 2013, 2014, 2015, 2016],   // Set X-axis labels
+    datasets: [{
+      label: 'Users',                         // Create the 'Users' dataset
+      data: [120, 60, 50, 180, 120]           // Add data to the chart
+    }]
+  }
+});
+</script>
